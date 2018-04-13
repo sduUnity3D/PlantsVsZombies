5db1a46c96de37ba9598f6e897f9f8e4dba46637
Zonghe Yuan <qq295087430@outlook.com>
2018-04-13 21:49:45 +0800

    植物预览
    第一发是我的了hhhh

---
+++ Assets/plantscript/plantAttack.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plantAttack : MonoBehaviour {
+	private bool onAttack;
+	public LayerMask zombielayer;
+	public float attackspeed;
+	public float attackdistance;
+	private float attackcooldown;
+	public int planttype;
+	private float times;
+	// Use this for initialization
+	void Start () {
+		
+		onAttack = false;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		times = Time.time;
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, transform.right,10f,zombielayer);
+		Debug.DrawLine(transform.position,transform.position+new Vector3(attackdistance,0f,0f),Color.red);
+		if (hit.collider != null&&onAttack==false) {
+			onAttack = true;
+			attackcooldown = Time.time+attackspeed;
+		} else if(hit.collider==null){
+			onAttack = false;}
+		if (onAttack == true) {
+			if (attackcooldown < Time.time) {
+				attackcooldown = Time.time+attackspeed;
+				attack ();
+			}
+		}
+
+		
+
+
+	}
+	void FixedUpdate(){
+	
+	
+	}
+
+	void attack(){
+		GameObject hp_bar = (GameObject)Resources.Load("zidan");  
+		Instantiate(hp_bar);  
+		hp_bar.transform.position = gameObject.transform.position;
+		hp_bar.GetComponent<Rigidbody2D>().velocity = new Vector2 (5f, 0f);
+		hp_bar.GetComponent<zidanscript> ().shootdistance = 20f;
+		hp_bar.GetComponent<zidanscript> ().speed = 5;
+	}
+
+}
---
+++ Assets/plantscript/plantHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plantHealth : MonoBehaviour {
+	public float health;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void be_damage(float hurt){
+		health -= hurt;
+		if (health <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
